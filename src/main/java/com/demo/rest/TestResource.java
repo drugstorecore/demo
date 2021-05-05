fc58c9bdd2819dabbdbe9e1da6047b42c93f5bf3
Devalgas <kamgadevalgas@gmail.com>
2021-05-05 12:07:33 +0200

    jython

--- src/main/java/com/demo/rest/TestResource.java
+++ src/main/java/com/demo/rest/TestResource.java
@@ -5,28 +5,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-
 @RestController
 @RequestMapping("/api")
 public class TestResource {
+    private final PythonInterpreter pythonInterpreter;
+
+    public TestResource(PythonInterpreter pythonInterpreter) {
+        this.pythonInterpreter = pythonInterpreter;
+    }
 
 
     @GetMapping("/test")
-    public Boolean test() throws FileNotFoundException {
-        try (PythonInterpreter pyInterp = new PythonInterpreter()) {
-//            File initialFile = new File("src/main/resources/test.py");
-            //via file .py
-//            pyInterp.execfile(new FileInputStream(initialFile));
-
-            //via code
-            pyInterp.exec("print('Hello Python World!')");
-            return true;
-
-        }
-
+    public Boolean test() {
+        pythonInterpreter.exec("print('Hello Python World!')");
+        return true;
 
     }
 }
